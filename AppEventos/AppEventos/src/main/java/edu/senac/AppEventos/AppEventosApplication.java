87bd50255f7c38ecddbbec49f9c60e810b51b40e
Danilo Soares <danilo.silva@nutes.ufpe.br>
2022-11-24 11:46:51 -0300

    Inicio do projeto Spring

---
+++ AppEventos/AppEventos/src/main/java/edu/senac/AppEventos/AppEventosApplication.java
@@ -0,0 +1,13 @@
+package edu.senac.AppEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEventosApplication.class, args);
+	}
+
+}
